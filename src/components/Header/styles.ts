17c3625c074b1a0a6309e49ca831a5b444d575c9
Arthur Arend <arthur.nox@gmail.com>
2020-11-16 22:11:07 -0300

    Inicializando Repo

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Container = styled.header`
+
+  height: 80px;
+  width:100%;
+
+  background-color: blue;
+
+  display: flex;
+  justify-content: center;
+  align-items:center;
+
+`
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+import Header from '../../components/Header';
+import Menu from '../../components/Menu';
+
+const Home: React.FC = () => {
+  return (
+    <>
+      <Header />
+      <Menu />
+    </>
+
+  );
+}
+
+export default Home;
